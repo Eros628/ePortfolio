6cc29d8fa1d10c054b2849fed72ac04772599669
zayqrashid <eroslucagbo314@gmail.com>
2024-12-17 21:39:03 +0800

    Added some animation scroll in about page

---
+++ .history/script_20241217164227.js
@@ -0,0 +1,20 @@
+const carouselItems = document.querySelectorAll('.carousel-item');
+let currentIndex = 0;
+
+// Function to update center item
+function updateCarousel() {
+  carouselItems.forEach((item, index) => {
+    item.classList.remove('active');
+    if (index === currentIndex) {
+      item.classList.add('active');
+    }
+  });
+}
+
+// Example: Change center image every 2 seconds
+setInterval(() => {
+  currentIndex = (currentIndex + 1) % carouselItems.length;
+  updateCarousel();
+}, 2000);
+
+updateCarousel();

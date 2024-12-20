e76497c118d03e0444edecf8fca1c036fb62d5d7
zayqrashid <eroslucagbo314@gmail.com>
2024-12-21 00:36:16 +0800

    Polishing

--- script.js
+++ script.js
@@ -1,22 +1,21 @@
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener('DOMContentLoaded', () => {      
     const modal = document.querySelector(".screenBody");
     const overlay = document.querySelector(".overlay");
     const openModalBtn = document.querySelector(".email-box");
     const closeModalBtn = document.querySelector(".cancel");
 
-
-    const openModal = function () {
-        overlay.classList.remove("hidden");
+    const openModal = function () {             //open the modal window. Removing the hidden class.
+        overlay.classList.remove("hidden");   
         modal.classList.remove("hidden");
         gsap.from('.screenBody', {duration: 0.5, opacity:0, ease:'back-in'})
     };
 
-    const closeModal = function () {
+    const closeModal = function () {            //close the modal window, adding back the hidden class.
         modal.classList.add("hidden");
         overlay.classList.add("hidden");
       };
 
-    openModalBtn.addEventListener("click", (event) => {
+    openModalBtn.addEventListener("click", (event) => {  
         event.preventDefault();
         openModal();
     });
